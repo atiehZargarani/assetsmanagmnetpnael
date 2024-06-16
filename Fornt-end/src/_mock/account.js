14503b9f9021c7b7291a7b0e7e74d9c4753fef01
atieh <atiapj@gmail.com>
2024-06-16 14:15:05 +0330

    first commit

---
+++ Fornt-end/src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'نوید کیانوش',
+  email: 'demo@minimals.cc',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
